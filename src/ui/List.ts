e10db8b385f2176a61adc0a71a09372f2821e8d4
Victor <78041023+victor31657513@users.noreply.github.com>
2025-08-24 16:21:50 +0200

    feat: render routes with internal List component (#66)

---
+++ src/ui/List.ts
@@ -0,0 +1,30 @@
+export interface ListProps<T> {
+  /** Container element that will receive rendered list items */
+  container: HTMLElement
+  /**
+   * Callback used to render each item. It can return an HTMLElement
+   * synchronously or a Promise resolving to an HTMLElement.
+   */
+  renderItem: (item: T, index: number) => HTMLElement | Promise<HTMLElement>
+}
+
+/** Simple utility component to render a list of items inside a container */
+export class List<T> {
+  private container: HTMLElement
+  private renderItem: (item: T, index: number) => HTMLElement | Promise<HTMLElement>
+
+  constructor(props: ListProps<T>) {
+    this.container = props.container
+    this.renderItem = props.renderItem
+  }
+
+  /** Replace the current content of the container with the provided items */
+  async setItems(items: T[]): Promise<void> {
+    this.container.innerHTML = ''
+    for (let i = 0; i < items.length; i++) {
+      const el = await this.renderItem(items[i], i)
+      this.container.appendChild(el)
+    }
+  }
+}
+
